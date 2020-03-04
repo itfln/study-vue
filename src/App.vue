3dafe938ed04afae6ada6bc32fa9453f7fb1b6ec
itfln <it_fln@163.com>
2020-03-04 16:54:52 +0800

    Message

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CourseList :courses="courses"></CourseList>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CourseList from '@/components/CourseList.vue';
+import {getCourses} from "@/api/course"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    CourseList
+  },
+  data() {
+    return {
+       title:"开课吧购物车",
+       course:'',
+       courses:[]
+    }
+  },
+  async created(){
+      //组件实例已经创建，由于未挂载，dom不存在
+      const courses = await getCourses()
+      this.courses = courses
+  },
 }
 </script>
 
@@ -25,4 +38,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.icon{
+      width: 1em;
+      height: 1em;
+      vertical-align: -0.15em;
+      fill: currentColor;
+      overflow: hidden;
+      }
+
 </style>
---
+++ src/components/CourseList.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <!--条件渲染-->
+        <p v-if="courses.length == 0">没有任何课程信息</p>
+        <!--列表渲染-->
+        <!--
+        <div v-for="c in courses" :key="c" @click="selectedCourse = c" 
+        :class="{active:selectedCourse === c}">
+            {{ c}}
+        </div>>
+        -->
+        <!--style-->
+        <div class="course-list" v-else>
+            <div v-for="c in courses" :key="c.name" @click="selectedCourse = c" 
+            :style="{backgroundColor:selectedCourse===c?'#ddd':'transparent'}">
+            {{ c.name }} - ¥{{c.price}}
+            </div>
+        </div>    
+    </div>
+</template>
+
+<script>
+    export default {
+            data(){
+                return {
+                    selectedCourse:''
+                }
+            },
+            props:{
+                courses:{
+                    type:Array,
+                    default:function(){
+                        return []
+                    }
+                }
+            },
+            filters:{
+                currency(value,symbol="¥"){
+                    return symbol + value;
+                }
+            }
+        }
+</script>
+
+<style scoped>
+.active{
+    background-color: #ddd;
+}
+</style>
